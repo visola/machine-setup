092f9171cad6e71fcf23ac85cf734683f1585233
Vinicius Isola <viniciusisola@gmail.com>
2018-01-11 12:28:54 -0500

    Switch to scripts dir before sourcing

--- profile.sh
+++ profile.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+pushd $SCRIPT_DIR >> /dev/null
+
 source sync_dirs.sh
 source setup_git_info.sh
 source setup_autocomplete.sh
 source alias_node_files.sh
+
+popd >> /dev/null
 
 get-quote
