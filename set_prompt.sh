3c64565fa07aa680e6f3cf93bffb3b1b1def628a
Vinicius Isola <viniciusisola@gmail.com>
2020-01-14 15:09:23 -0500

    Fix set_prompt

--- set_prompt.sh
+++ set_prompt.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-runPrompt() {
-  prompt
-}
-# export PS1="\n---- \u @ \h \$(git-info) [\t] ---- \n \w $ "
-export PS1='$(runPrompt)'
+export PS1="\n---- \u @ \h \$(git-info) [\t] ---- \n \w $ "
