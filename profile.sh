eb44559d4aee54adc77bce8abd179633a5c3e2dc
Vinicius Isola <viniciusisola@gmail.com>
2020-10-26 19:51:56 -0400

    Add support to zsh

--- profile.sh
+++ profile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPT_DIR="$( cd "$( dirname "$0}" )" && pwd )"
 
 pushd $SCRIPT_DIR >> /dev/null
 
--- set_prompt.sh
+++ set_prompt.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-export PS1="\n---- \u @ \h \$(git-info) [\t] ---- \n \w $ "
+if [ -n "$ZSH_VERSION" ]; then
+    NEWLINE=$'\n'
+    export PS1="${NEWLINE}---- %(?.%F{green}√.%F{red}%?)%f %n \$(git-info) [%*] ---- ${NEWLINE} %B%3~%b $ "
+elif [ -n "$BASH_VERSION" ]; then
+    export PS1="\n---- \u @ \h \$(git-info) [\t] ---- \n \w $ "
+fi
