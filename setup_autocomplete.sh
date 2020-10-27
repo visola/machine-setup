05cdbd036e04a6824ef2df1d93a407056d419bd9
Vinicius Isola <viniciusisola@gmail.com>
2020-10-26 21:34:57 -0400

    Really fix git autocomplete for zsh

--- setup_autocomplete.sh
+++ setup_autocomplete.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 if [ -d ~/autocomplete/ ]; then
-  if [ -n "$BASH_VERSION" ]; then
+  if [ -n "$ZSH_VERSION" ]; then
+    for f in ~/autocomplete/*.zsh; do
+      source $f > /dev/null 2>&1
+    done
+  elif [ -n "$BASH_VERSION" ]; then
     for f in ~/autocomplete/*.bash; do
       . $f
     done
