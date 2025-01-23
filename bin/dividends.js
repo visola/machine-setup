746babbe355015ef948bd0d60d4536f42e539760
Vinicius Isola <vini@fossa.com>
2025-01-23 17:43:45 -0500

    Add more tickers

--- bin/dividends.js
+++ bin/dividends.js
@@ -38,7 +38,7 @@ const roundNumber = (number) => {
 const main = async () => {
     const today = new Date();
     const lastYear = today.getFullYear() - 1;
-    const TICKERS_TO_ANALYZE = ['FE', 'JNJ', 'JEPQ', 'KBWD', 'MAXI', 'O', 'QYLE', 'SDIV', 'STWD', 'TSLY', 'ULTY', 'VZ', 'XYLD'];
+    const TICKERS_TO_ANALYZE = ['FE', 'JNJ', 'JEPI', 'JEPQ', 'KBWD', 'MAXI', 'O', 'QYLE', 'SCHD', 'SDIV', 'SPYD', 'STWD', 'TSLY', 'ULTY', 'VZ', 'XYLD'];
     for (let ticker of TICKERS_TO_ANALYZE) {
         const history = await get(`https://financialmodelingprep.com/api/v3/historical-price-full/stock_dividend/${ticker}?apikey=${API_KEY}`);
         const groupedByYear = groupByYear(history.historical);
